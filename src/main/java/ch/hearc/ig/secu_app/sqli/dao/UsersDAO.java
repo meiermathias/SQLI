c694736297325ce7a93d29b6e5c9b91c96cb36c5
meiermathias <mathiasmeier.11@gmail.com>
2017-05-16 19:14:24 +0200

    style

--- src/main/java/ch/hearc/ig/secu_app/sqli/dao/UsersDAO.java
+++ src/main/java/ch/hearc/ig/secu_app/sqli/dao/UsersDAO.java
@@ -12,7 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import oracle.jdbc.driver.OracleDriver;
 /**
  *
  * @author tim.sermier
